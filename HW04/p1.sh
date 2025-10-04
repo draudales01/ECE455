6fd0dafbd81477c126d00b26448e4a4f8fbbb74c
draudales01 <raudales@wisc.edu>
2025-10-03 19:20:22 -0500

    lab4

---
+++ HW04/p1.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+#SBATCH --partition=instruction
+#SBATCH --time=00:01:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=1
+#SBATCH --job-name=p1
+#SBATCH --time=00:01:00
+#SBATCH --output=hello.output
+
+module load nvidia/cuda
+nvcc hello.cu -o hello 
+./hello
